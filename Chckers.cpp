6a71cd8eb8fae88db91be49c93dd433f945a81e4
james-bruceLSL <james.bruce@londonstructureslab.com>
2025-07-07 22:29:00 +0100

    First pass - using standard libraries to create a checkerboard

--- Chckers.cpp
+++ Chckers.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World" << std::endl;
+using namespace std;
+
+int main() {
+    int rows = 8;
+    int cols = 8;
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            // Alternate between '#' and ' ' depending on the sum of row and column indexes
+            if ((i + j) % 2 == 0)
+                cout << "#";
+            else
+                cout << "o";
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
